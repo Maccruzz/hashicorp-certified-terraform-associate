1178c771919a1755ff619462e62b1e601335efa3
Maccruzz <soopaulfulbert08@gmail.com>
2024-01-29 16:34:37 +0400

    new

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 # Resource Block
-resource "aws_instance" "terraform_test_vm" {
+resource "aws_instance" "terraform_test_vm1" {
   ami           = "ami-0fef2f5dd8d0917e8" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
